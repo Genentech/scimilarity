3e2009bd1cc1dd6aa907bc92cec6b5bb49b991a0
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2024-03-21 15:39:24 -0700

    Add custom css for nbsphinx rendering.

--- docs/conf.py
+++ docs/conf.py
@@ -189,6 +189,9 @@ html_theme = "pydata_sphinx_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+# Custom CSS
+html_css_files = ['custom.css']
+
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
 # html_last_updated_fmt = '%b %d, %Y'
