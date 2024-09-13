d2b8364cb355c22e5403cbe0eeadf6c2011d0788
tony-kuo <tony.cy.kuo@gmail.com>
2024-09-13 07:13:52 -0700

    fix import in consolidate dup symbols

--- src/scimilarity/utils.py
+++ src/scimilarity/utils.py
@@ -207,6 +207,7 @@ def consolidate_duplicate_symbols(
 
     import anndata
     from collections import Counter
+    import numpy as np
     import pandas as pd
     from scipy.sparse import csr_matrix
 
